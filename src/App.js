af6caaaddb2103b48be744a8e01af9a7420f9235
System Administrator <root@nycc02r59chg8wp.aedr.us.aexp.com>
2018-01-05 15:19:18 -0500

    added book selves as a separate component

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import BooksSelf from './BooksSelf'
+
+class BooksApp extends React.Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+
+    books: [],
+    showSearchPage: false
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books : books
+      })
+
+    })
+  }
+
+  moveBook = (book, value) => {
+
+    BooksAPI.update(book, value)
+
+    var updatebooks = this.state.books
+
+    var i = updatebooks.findIndex(b => b.id === book.id)
+    updatebooks[i].shelf = value;
+
+    this.setState((state) => ({
+      books: updatebooks
+    }))
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        ) : (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <BooksSelf
+                  books={this.state.books.filter(book => book.shelf === "currentlyReading")}
+                  title="Currently Reading"
+                  onMoveBook={this.moveBook}
+                />
+                <BooksSelf
+                  books={this.state.books.filter(book => book.shelf === "wantToRead")}
+                  title="Want to Read"
+                  onMoveBook={this.moveBook}
+                />
+                <BooksSelf
+                  books={this.state.books.filter(book => book.shelf === "read")}
+                  title="Read"
+                  onMoveBook={this.moveBook}
+                />
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BooksApp
